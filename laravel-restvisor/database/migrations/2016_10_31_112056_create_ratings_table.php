7b728cb532353bdd8c91cad9137208afdf5905b6
Francisco Neves <fntneves@gmail.com>
2016-10-31 11:23:23 +0000

    Add migrations and models

---
+++ laravel-restvisor/database/migrations/2016_10_31_112056_create_ratings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->enum('score', [1, 2, 3, 4, 5]);
+            $table->string('comment')->nullable();
+            $table->integer('restaurant_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('restaurant_id')->references('id')->on('restaurants');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ratings');
+    }
+}
